24ca311fef6cff206e0cab6ff98e74872af3fb47
Rinat Ibragimov <ibragimov.rinat.810@yandex.com>
2024-12-11 17:39:32 +0500

    added logic

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,1 +1,45 @@
 #!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import { randomIntegerFunc } from '../src/index.js';
+
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
+
+const brainProgression = (UserName, progressionLength = 10) => {
+  console.log('What number is missing in the progression?');
+  let i = 1;
+  const progression = [];
+
+  while (i < 4) {
+    const firstNumberOfProgression = randomIntegerFunc(0, 9);
+    const mathProgressionStep = randomIntegerFunc(0, 9);
+
+    for (let j = 0; j <= progressionLength; j += 1) {
+      const currentNumber = firstNumberOfProgression + j * mathProgressionStep;
+      progression[j] = currentNumber;
+    }
+
+    const missingNumberIndex = randomIntegerFunc(0, 9);
+    const missingNumber = progression[missingNumberIndex];
+    progression[missingNumberIndex] = '..';
+
+    console.log(`Question: ${progression.join(' ')}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (Number(userAnswer) !== missingNumber) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${missingNumber}'.`);
+      console.log(`Let's try again, ${UserName}!`);
+      return;
+    }
+
+    i += 1;
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${UserName}!`);
+};
+
+brainProgression(name);
