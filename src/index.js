1176bc482ac75e2f9b41fe590e947861054cc4ba
Rinat Ibragimov <ibragimov.rinat.810@yandex.com>
2024-12-11 14:39:26 +0500

    added isEven func bcs it is commonfunc

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import readlineSync from 'readline-sync';
+
+const randomIntegerFunc = (min = 0, max = 100) => {
+  // случайное число от min до (max+1)
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
+
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
+
+export { randomIntegerFunc, greeting };
