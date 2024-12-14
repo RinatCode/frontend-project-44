f3084c9c5b28b0f4eb274f8d4ba601f8628919d9
Rinat Ibragimov <ibragimov.rinat.810@yandex.com>
2024-12-14 09:45:14 +0500

    random number range reduced from 0 - 10 to 0 - 10

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const randomIntegerFunc = (min = 0, max = 100) => {
+const randomIntegerFunc = (min = 0, max = 10) => {
   // случайное число от min до (max+1)
   const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
