f7709b62fc2dd4fa217ccdaebebe71f545f6e92b
Rinat Ibragimov <ibragimov.rinat.810@yandex.com>
2024-12-17 10:10:45 +0500

    fixed grammar mistake

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -6,7 +6,7 @@ const answer = () => readlineSync.questionInt('Your answer: ');
 const questionAndCorrectAnswerFunc = () => {
   const num1 = randomIntegerFunc();
   const num2 = randomIntegerFunc();
-  const question = `Ques1tion: ${num1} ${num2}`;
+  const question = `Question: ${num1} ${num2}`;
   const correctAnswer = gcd(num1, num2);
 
   return [question, correctAnswer];
